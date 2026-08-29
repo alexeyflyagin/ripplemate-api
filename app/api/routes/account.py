d2b0ee22c1283bfe910e760cc14fdf77baf6f310
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-29 17:28:20 +0200

    feat(AccountRead): add email and is_verified fields into AccountRead schema

--- app/api/routes/account.py
+++ app/api/routes/account.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter, Depends
 
+from app.models import User
 from app.models.account import Account
 from app.schemas.account import AccountRead
-from app.users import get_current_account
+from app.users import get_current_account, current_active_user
 
 router = APIRouter()
 
 
 @router.get("", response_model=AccountRead)
-async def get_my_account(account: Account = Depends(get_current_account)):
-    return AccountRead.model_validate(account)
+async def get_my_account(
+        user: User = Depends(current_active_user),
+        account: Account = Depends(get_current_account),
+):
+    account_dict = account.__dict__.copy()
+    account_dict["email"] = user.email
+    account_dict["is_verified"] = user.is_verified
+
+    return AccountRead.model_validate(account_dict)
--- app/schemas/account.py
+++ app/schemas/account.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
 
 class AccountRead(BaseModel):
     id: str = Field(validation_alias="public_id")
     display_name: str
+    email: EmailStr
+    is_verified: bool
     created_at: datetime
 
     model_config = ConfigDict(from_attributes=True, populate_by_name=True)
